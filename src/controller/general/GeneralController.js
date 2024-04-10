0aff403f4a0cb86ddad1ea9bc30fd32947164997
Sharduli  Pandey <shardulipandey8090@gmail.com>
2024-04-10 13:45:53 +0530

    fixed errors

--- src/controller/general/GeneralController.js
+++ src/controller/general/GeneralController.js
@@ -1,13 +1,10 @@
-import argon2 from "argon2";
-import jwt from "jsonwebtoken";
-import { employeedatacollection, ticketdatacollection } from "../../config.js";
-import shortid from "shortid";
+import { querydatacollection } from "../../config.js";
 
 export class GeneralController {
     async sendQueryFeedback(request, response, next) {
         try {
             let { name, query } = request.body;
-            const data = await querydatacollection.insertOne({name , query});
+            const data = await querydatacollection.insertOne({ name, query });
 
             response.json({
                 statuscode: 200,
